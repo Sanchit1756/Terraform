0b1b864029896b4997739d7246bd953f552f818a
Pr4Mukh <62900685+Sanchit1756@users.noreply.github.com>
2024-06-02 10:35:19 +0530

    Terraform code

---
+++ vpc.tf
@@ -0,0 +1,38 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.51.1"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-2"
+}
+
+/*resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "Terraform_vpc"
+  }
+}
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "igw_terraform"
+  }
+}*/
+
+resource "aws_instance" "demoinstace" {
+
+  ami = "ami-0ca2e925753ca2fb4"
+  instance_type = "t2.micro"
+
+}
+
+output "insatance_id" {
+  value = aws_instance.demoinstace.arn 
+  
+}
