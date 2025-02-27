0641c0ebd6faf4de5506bfbb2ba6f47809273015
Pr4Mukh <62900685+Sanchit1756@users.noreply.github.com>
2025-02-28 00:43:53 +0530

    Update vpc.tf

--- VPC_code/vpc.tf
+++ VPC_code/vpc.tf
@@ -11,7 +11,7 @@ provider "aws" {
   region = "us-east-2"
 }
 
-/*resource "aws_vpc" "main" {
+resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
   tags = {
     Name = "Terraform_vpc"
@@ -23,16 +23,5 @@ resource "aws_internet_gateway" "gw" {
   tags = {
     Name = "igw_terraform"
   }
-}*/
-
-resource "aws_instance" "demoinstace" {
-
-  ami = "ami-0ca2e925753ca2fb4"
-  instance_type = "t2.micro"
-
 }
 
-output "insatance_id" {
-  value = aws_instance.demoinstace.arn 
-  
-}
